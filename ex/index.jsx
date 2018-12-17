0ddfbaf65d5f273f5d77a4bdd4e0892603f26271
Fernando Lopes <nandolopis@gmail.com>
2018-12-17 14:09:45 -0200

    Aula  Importância da referência

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,4 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
-
-const logger = require('./logger')
-logger.info('Usando o padrão CommonJS!')
-
+/**
+ * no webpack so será carregado atravez do arquivo de referência 
+ */
+require('./duvidaCruel')
